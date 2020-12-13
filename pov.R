9d7e0de1e5a220b19016e4a05cd794610fa5a6b9
jam <jam@pop-os.localdomain>
2020-12-13 00:10:17 +0000

    Working on layout

--- pov.R
+++ pov.R
@@ -8,15 +8,11 @@ library(shiny)
 library(shinydashboard)
 library(plotly)
 library(wbstats)
-library(rmapshaper)
-library(sp)
-library(geojsonio)
-
 
 ## import poverty data
 pov_dat <- povcalnet(
   fill_gaps = T,
-  year = c(1980:2019)
+  year = c(1990:2019)
 )
 povcal_extra <- povcalnet_info() %>% as_tibble()
 pov_extra_trim <- povcal_extra %>% select(country_code, wb_region) ## can also get more detailed region
@@ -50,7 +46,7 @@ my_indicators <- c(
   pop = "SP.POP.TOTL"
 )
 
-wb_dat <- wb_data(my_indicators, start_date = 1980, end_date = 2019)
+wb_dat <- wb_data(my_indicators, start_date = 1990, end_date = 2019)
 
 ## changed left join to right join
 dat_j <-
@@ -80,51 +76,6 @@ dat_map <-
   rwm_tidy %>%
   left_join(dat_j, by = c("ISO3" = "countrycode")) %>%
   filter(id != "Antarctica")
-
-## this is all redundant now I think
-## deal with the ones who wouldnt gather data and have no years
-# no_dat_collect <-
-# dat_map %>%
-# filter(is.na(countryname), id != "Antarctica")
-#
-# years_available <- na.omit(unique(dat_map$year))
-#
-# nrow(no_dat_collect)
-#
-# for (i in 1:length(years_available)) {
-# no_dat_collect <-
-# no_dat_collect %>%
-# add_column(
-# x = years_available[i],
-# .name_repair = "unique"
-# )
-# }
-# no_dat_fix <-
-# no_dat_collect %>%
-# pivot_longer(cols = x...21:x...58) %>%
-# select(-year, -name) %>%
-# rename(year = "value")
-#
-# dat_map_fix <-
-# dat_map %>%
-# filter(!is.na(countryname)) %>%
-##  bind_rows(no_dat_fix) %>%
-# select(-countryname, -iso2c)
-#
-### no fill in for countries
-# dat_map_fix <-
-# dat_map_fix %>%
-# group_by(group) %>%
-# complete(year = 1981:2019, nesting(long, lat))
-#
-# dat_map_fix %>% filter(ISO3 == "SSD")
-# expand(dat_map_fix, nesting(group), year = 1981:2019)
-
-## join up map data
-# dat_map <-
-# world_reduce %>%
-# inner_join(dat_j, by = c("region" = "countryname"))
-
 write_csv(dat_map, "test_data_small.csv")
 
 ## variables
@@ -148,13 +99,41 @@ pov_data_go <- pov_data %>% filter(year == 2000)
 map_plot <-
   pov_data_go %>%
   ggplot(aes(x = long, y = lat, group = group, text = country, fill = headcount)) +
-  geom_polygon(colour = "grey80", size = 0.1) +
+  geom_polygon(colour = "grey80", size = 1) +
   scale_fill_continuous(na.value = "grey70")
-map_plot
 
+## plotly highlight testing
+map_plotly <- ggplotly(map_plot)
+
+s <- attrs_selected(
+  showlegend = TRUE,
+  mode = "lines+markers",
+  marker = list(symbol = "x")
+)
+
+highlight(map_plotly,
+  on = "plotly_click",
+  color = toRGB("red"),
+  selected = s
+)
+
+## line chart
+pov_data %>%
+  filter(id == "Ireland") %>%
+  ggplot(aes(x = year, y = headcount, colour = id)) +
+  geom_point() +
+  geom_path() +
+  theme_classic()
+
+## gapminder
+pov_data_go %>%
+  group_by(id) %>%
+  top_n(1) %>%
+  ggplot(aes(x = gdp_capita, y = gini, colour = wb_region, size = population)) +
+  geom_point()
 
 # note curve number is position in list -1
-str_extract(plotly_test$x$data[[10]]$text, "[^<]+")
+str_extract(map_plotly$x$data[[10]]$text, "[^<]+")
 
 
 countries_vector <- vector("character", nrow(d))
@@ -170,6 +149,13 @@ pov_dat_hist <- pov_dat_go %>% filter(pov_data_go %>% countries_vector())
 hist(pov_dat_hist)
 
 ## shiny time
+
+ani_opts <-
+  animationOptions(
+    interval = 4000,
+    playButton = "Play",
+    pauseButton = "Pause"
+  )
 
 ## Sidebar content
 ui <- dashboardPage(
@@ -187,7 +173,7 @@ ui <- dashboardPage(
       min = 1990,
       max = 2019,
       value = 1990,
-      animate = T
+      animate = ani_opts
     ),
     # dateRangeInput("daterange", "Select years:",
     # startview = "decade",
@@ -196,37 +182,61 @@ ui <- dashboardPage(
     # min = "1967",
     # max = "2019"
     # ),
-    textInput("text", "Text input:")
+    ## textInput("text", "Text input:")
+    selectInput(
+      "var_primary", "Primary variable:",
+      c(
+        "Extreme poverty %" = "headcount",
+        "Watt's Poverty Index" = "watts",
+        "Gini Inequality Index" = "gini",
+        "Life expectancy" = "life_exp",
+        "GDP per capita" = "gdp_capita"
+      )
+    ),
+    selectInput(
+      "var_secondary", "Secondary variable",
+      c(
+        "Extreme poverty %" = "headcount",
+        "Watt's Poverty Index" = "watts",
+        "Gini Inequality Index" = "gini",
+        "Life expectancy" = "life_exp",
+        "GDP per capita" = "gdp_capita"
+      )
+    ),
+
+    tableOutput("data")
   )),
   dashboardBody(
     # Boxes need to be put in a row (or column) tabItems(
-    # First tab content
-    tabItem(
-      tabName = "dashboard",
-      fluidRow(
-        box(
-          title = "Histogram",
-          status = "primary",
-          solidHeader = TRUE,
-          background = "light-blue",
-          plotlyOutput("plot1", height = 600)
-        )
+    fluidRow(
+      box(
+        title = "World map",
+        status = "primary",
+        solidHeader = TRUE,
+        background = "light-blue",
+        plotlyOutput("plot1", height = 600)
+      ),
+      box(
+        title = "Gapminder",
+        solidHeader = TRUE,
+        background = "light-blue",
+        plotlyOutput("gmplot", height = 600)
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Country data",
+        solidHeader = T
       ),
       box(
         title = "Line graph",
         solidHeader = T,
-        plotOutput("plot2", height = 300)
+        plotlyOutput("plot2", height = 300)
       ),
       box(
         title = "debug",
         verbatimTextOutput("clicking")
       )
-    ),
-
-    # Second tab content
-    tabItem(
-      tabName = "widgets",
-      h2("Widgets tab content")
     )
   )
 )
@@ -236,59 +246,109 @@ server <- function(input, output) {
   ## read in the data
   pov_data <- read.csv("test_data_small.csv") %>% as_tibble()
 
-  output$plot1 <- renderPlotly({
+  ## transform to non-map data
+  pov_data_nomap <-
+    pov_data %>%
+    group_by(group, year) %>%
+    slice_head(n = 1)
+
+  ## create a reactive object to pass elsewhere
+  map_r <- reactive({
     pov_data_go <- pov_data %>% filter(year == input$slider)
 
     map_plot <-
       pov_data_go %>%
-      ggplot(aes(x = long, y = lat, group = group, text = id, fill = headcount)) +
-      geom_polygon(colour = "grey80", size = 0.1)
+      ggplot(aes(x = long, y = lat, group = group, text = id)) +
+      geom_polygon(aes_string(fill = input$var_primary), colour = "grey80", size = 0.1)
 
-
-    map_plotly <-
-      map_plot %>%
-      ggplotly() %>%
-      event_register("plotly_click")
+    map_plotly <- ggplotly(map_plot)
 
     map_plotly
   })
 
-  output$plot2 <- renderPlot({
+  output$plot1 <- renderPlotly({
+    # pov_data_go <- pov_data %>% filter(year == input$slider)
+    #
+    # map_plot <-
+    # pov_data_go %>%
+    # ggplot(aes(x = long, y = lat, group = group, text = id)) +
+    # geom_polygon(aes_string(fill = input$var_primary), colour = "grey80", size = 0.1)
+    #
+    # map_plotly <- ggplotly(map_plot)
+    map_r() %>% event_register("plotly_click")
+    ## map_data
+
+    # map_plotly <- plot_geo(pov_data_go)
+    # map_plotly <- map_plotly %>% add_trace(
+    # z = ~headcount, text = ~id, color = ~headcount, locations = ~ISO3, colors = "Purples"
+    # )
+
+    # highlight_key() # this might be the answer
+    # highligh deosnt seem to work
+    # highlight(map_plotly,
+    # on = "plotly_click",
+    # color = toRGB("red")
+    # ) %>%
+    # event_register("plotly_click")
+  })
+
+  # gapminder style visual
+  output$gmplot <- renderPlotly({
+    data_gm <- pov_data_nomap %>% filter(year == input$slider)
+
+    wb_plot <-
+      ggplot(data_gm, aes_string(
+        x = input$var_primary,
+        y = input$var_secondary,
+        text = "id"
+      )) +
+      geom_point(aes(colour = wb_region, size = population))
+
+    ggplotly(wb_plot, tooltip = "text") %>% event_unregister("plotly_click")
+  })
+
+  # line plot over year
+  output$plot2 <- renderPlotly({
     d <- event_data("plotly_click")
 
-    # if (is.null(d)) {
-    # hist(pov_data$mean)
-    # } else {
-    # (hist(d$mean))
-    # }
-    ## repeating this
-    pov_data_go_2 <- pov_data %>% filter(year == input$slider)
+    # pov_data_go_2 <- pov_data %>% filter(year == input$slider)
+    #
+    # map_plot_2 <-
+    # pov_data_go_2 %>%
+    # ggplot(aes(x = long, y = lat, group = group, text = id)) +
+    # geom_polygon(aes_string(fill = input$var_primary))
+    #
+    # map_plotly_2 <-
+    # map_plot_2 %>%
+    # ggplotly()
 
-    map_plot_2 <-
-      pov_data_go_2 %>%
-      ggplot(aes(x = long, y = lat, group = group, text = id, fill = headcount)) +
-      geom_polygon(colour = "grey80", size = 0.1)
 
-    map_plotly_2 <-
-      map_plot_2 %>%
-      ggplotly()
-
+    ## extract out the clicked country (I think)
     countries_vector <- c()
 
     for (i in 1:length(d)) {
       foo_curve <- d$curveNumber[i] + 1
-      foo_country <- str_extract(map_plotly_2$x$data[[foo_curve]]$text, "[^<]+")
+      foo_country <- str_extract(map_r()$x$data[[foo_curve]]$text, "[^>]*$")
       countries_vector[i] <- foo_country
     }
 
-    pov_data_hist <- pov_data_go_2 %>% filter(id %in% countries_vector)
+    pov_data_line <- pov_data_nomap %>% filter(id %in% countries_vector)
 
-    hist(pov_data_hist$headcount)
+    line_plot <-
+      ggplot(
+        pov_data_line,
+        aes_string(y = input$var_primary)
+      ) +
+      geom_line(aes(x = year, colour = id))
+
+    ggplotly(line_plot) %>% event_unregister("plotly_click")
   })
 
   output$clicking <- renderPrint({
     d <- event_data("plotly_click")
-    d
+    txt <- input$var_primary
+
+    paste(d, txt)
 
     ## hist(df_select$mean)
   })
@@ -302,5 +362,14 @@ server <- function(input, output) {
   ## output$plot2 <- renderPlot({
 }
 
+s_dash <- shinyApp(ui, server)
+s_dash
 
-shinyApp(ui, server)
+
+## experiments
+# fig <- plot_geo(pov_data_go)
+# fig2 <- fig %>% add_trace(
+# z = ~headcount, text = ~id, color = ~headcount, locations = ~ISO3, colors = "Purples"
+# ) %>% partial_bundle()
+# fig2
+# df <- read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
